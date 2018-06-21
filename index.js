a2f9e53792a5695dc874e71bb8ec2f6a4cf30f31
Nik <nik@iMac.local>
2018-06-21 10:22:25 +0300

    Базовые типы, массивы

---
+++ index.js
@@ -0,0 +1,7 @@
+var str = 'string!';
+var num = 10;
+var isTrue = true;
+var unknown = '1';
+unknown = true;
+var noName = 10;
+noName = 'apple';
---
+++ index.ts
@@ -0,0 +1,21 @@
+let str: string = 'string!';
+let num: number = 10;
+let isTrue: boolean = true;
+let unknown: any = '1';
+unknown = true;
+let noName: string | number = 10;
+noName = 'apple';
+
+let numArray1: number[] = [1, 2, 3];
+let numArray2: Array<number> = [1, 2, 3];
+
+
+let strArray1: string[] = ['1', '2', '3'];
+let strArray2: Array<string> = ['1', '2', '3'];
+
+let booArray1:boolean[] = [trrue, false];
+let booArray2:Array<boolean> = [trrue, false];
+
+// tuples
+let array1: [number, number, string] = [1, 2, '3'];
+let array2: [boolean,  string, number] = [true, 'str', 33];
