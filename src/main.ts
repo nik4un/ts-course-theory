c6870c130a65e1e2104259fa440f1f45760ffdf2
Nik <nik@iMac.local>
2018-06-22 08:52:28 +0300

    Специальные типы. Enum, never, null

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,30 @@
-type User = { name: string, age: number, logName: () => void, jobs: string[], getJobs?: () => string[] };
-
-let user: User =
-  {
-    name: 'Grey',
-    age: 50,
-    logName(): void {
-      console.log(this.name);
-    },
-    jobs: ['a', 'b'],
-    getJobs(): string[] {
-        return this.jobs;
+//тип "enum" служит в основном для задания констант
+enum Job {
+  Frontend,
+  Backend,
+  Designer = 50,
+  Manager
 }
-  };
+const job1: Job = Job.Frontend;
+console.log(job1); // 0
+const job2: Job = Job.Backend;
+console.log(job2); // 1
+const job3: Job = Job.Designer;
+console.log(job3); // 50
+const job4: Job = Job.Manager;
+console.log(job4); // 51
 
-let user2: User =
-  {
-    name: 'Max',
-    age: 30,
-    logName(): void {
-      console.log(this.name);
-    },
-    jobs: ['c', 'd'],
-  };
+// тип "null"
+let myNum: number | null = 20;
+console.log(myNum);
+myNum = null;
+console.log(myNum);
+myNum = undefined;
+console.log(myNum);
 
-console.log(user, user2);
+// для функций выбрасывающих ошибки, но ничего не возвращающих
+// придуман тип "never"
+const throwNewError = (err: string): never => {
+  throw new Error(err);
+};
+throwNewError('Ups!');
