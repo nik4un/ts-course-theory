24fc420cddf0e4bad85fce57600476da94deac25
Nik <nik@iMac.local>
2018-06-24 21:20:49 +0300

    Namespace

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,19 @@
-// декоратор - это функция, которая принимает в себя конструктор класса, за которым он закреплен
-// декоратор не может иметь аргументов
-function logger(constrFn: Function) {
-  console.log(constrFn);
-}
-function shouldLog(flag: boolean): any {
-  return flag ? logger : null;
-}
-
-@shouldLog(false)
-class User1 {
-  constructor(public name: string, public  age: number) {
-    console.log('I am nw user');
+// по-умолчанию всё содержимое namespace это - private
+namespace Util {
+  export function isEmpty(d: any) { // export делает снщность public
+    return !d;
   }
-}
 
-// =================================
-
-function addShowAbility(constructorFn: Function) {
-  constructorFn.prototype.showHtml = function () {
-    const pre = document.createElement('pre');
-    pre.innerHTML = JSON.stringify(this);
-    document.body.appendChild(pre);
+  function isUndefined(d: any) {
+    return typeof d === 'undefined';
   }
+  export const PI = Math.PI;
+  const EXP = Math.E;
 }
+console.log(Util.isEmpty(''));
+console.log(Util.isEmpty('str'));
+console.log(Util.PI);
 
-@addShowAbility
-class User2 {
-  constructor(public name: string, public  age: number, public job : string) {};
-}
+const EXP = 'Test';
 
-const user = new User2('Tom', 25, 'Aviator');
-console.log(user);
-(<any>user).showHtml();
+console.log(Math.E);
